5368e499332a520e52a5f90e5fc86310ec42a687
ohanssen <ohanssen@acm.org>
2020-09-16 23:30:40 +0200

    simple fifo impl

--- components/aprs/fifo.h
+++ components/aprs/fifo.h
@@ -17,33 +17,9 @@ typedef struct _fifo_t {
 
 
 
-inline void fifo_init(fifo_t* f, int8_t* buffer, uint8_t size) {
-  f->buffer = buffer; 
-  f->size = size;
-  f->len = 0;
-  f->pos = 0;
-}
-
-
-
-inline void fifo_push(fifo_t* f, int8_t x) {
-  if (f->len >= f->size)
-    return;
-  f->len++;
-  register int8_t i = (f->pos + f->len) % f->size; 
-  f->buffer[i] = x; 
-}
-
-
-
-
-inline int8_t fifo_pop(fifo_t* f) {
-  if (f->len == 0)
-     return 0;
-  f->len--;
-  f->pos = (f->pos + 1) % f->size;
-  return f->buffer[f->pos]; 
-}
+void fifo_init(fifo_t* f, int8_t* buffer, uint8_t size);
+void fifo_push(fifo_t* f, int8_t x);
+int8_t fifo_pop(fifo_t* f);
 
 
 #endif
