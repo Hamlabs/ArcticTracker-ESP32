fd6a42ff6e59583db202fb6d6589117878373d9c
LA7ECA, Øyvind <oh@hans.priv.no>
2024-02-24 21:05:57 +0100

    Use legacy ADC driver for now...

--- main/adc.c
+++ main/adc.c
@@ -4,13 +4,10 @@
 #include "driver/gpio.h"
 #include "defines.h"
 #include "config.h"
-
-#include "esp_adc/adc_oneshot.h"
-#include "esp_adc/adc_cali.h"
-#include "esp_adc/adc_cali_scheme.h"
-
-
 #include "ui.h"
+#include "hal/adc_hal.h"
+#include "esp_adc_cal.h"
+
 
 #define TAG "adc"
 
@@ -19,70 +16,7 @@
 
 static uint16_t dcoffset = 0;
 
-                          
-
-static bool adc_calibration_init(adc_unit_t unit, adc_atten_t atten, adc_cali_handle_t *out_handle)
-{
-    adc_cali_handle_t handle = NULL;
-    esp_err_t ret = ESP_FAIL;
-    bool calibrated = false;
-
-#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
-    if (!calibrated) {
-        ESP_LOGI(TAG, "calibration scheme version is %s", "Curve Fitting");
-        adc_cali_curve_fitting_config_t cali_config = {
-            .unit_id = unit,
-            .atten = atten,
-            .bitwidth = WIDTH,
-        };
-        ret = adc_cali_create_scheme_curve_fitting(&cali_config, &handle);
-        if (ret == ESP_OK) {
-            calibrated = true;
-        }
-    }
-#endif
-
-#if ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
-    if (!calibrated) {
-        ESP_LOGI(TAG, "calibration scheme version is %s", "Line Fitting");
-        adc_cali_line_fitting_config_t cali_config = {
-            .unit_id = unit,
-            .atten = atten,
-            .bitwidth = WIDTH,
-        };
-        ret = adc_cali_create_scheme_line_fitting(&cali_config, &handle);
-        if (ret == ESP_OK) {
-            calibrated = true;
-        }
-    }
-#endif
-
-    *out_handle = handle;
-    if (ret == ESP_OK) {
-        ESP_LOGI(TAG, "Calibration Success");
-    } else if (ret == ESP_ERR_NOT_SUPPORTED || !calibrated) {
-        ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
-    } else {
-        ESP_LOGE(TAG, "Invalid arg or no memory");
-    }
-
-    return calibrated;
-}
-
-
-static void adc_calibration_deinit(adc_cali_handle_t handle)
-{
-#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
-    ESP_LOGI(TAG, "deregister %s calibration scheme", "Curve Fitting");
-    ESP_ERROR_CHECK(adc_cali_delete_scheme_curve_fitting(handle));
-
-#elif ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
-    ESP_LOGI(TAG, "deregister %s calibration scheme", "Line Fitting");
-    ESP_ERROR_CHECK(adc_cali_delete_scheme_line_fitting(handle));
-#endif
-}
-
-
+// FIXME: Use new ADC driver and continous ADC mode for sampling RX
 
 
 #if DEVICE == T_TWR
@@ -95,7 +29,6 @@ void radio_adc_init()
 {
     /* Calibrate radio channel input */
     dcoffset = radio_adc_sample(RADIO_INPUT);
-    printf("DCOFFSET=%d\n", dcoffset);
 }
 
 
@@ -127,39 +60,57 @@ int16_t radio_adc_sample()
 
 #define ATTEN         ADC_ATTEN_DB_11
 #define WIDTH         ADC_BITWIDTH_12
-#define BATT_DIVISOR  3.3
+#define DEFAULT_VREF  1100
+#define BATT_DIVISOR  3
+#define UNIT          ADC_UNIT_1
+
+static esp_adc_cal_value_t char_type;
+static esp_adc_cal_characteristics_t *adc_chars;
+
+void adc_calibrate();
 
 
-static adc_oneshot_unit_handle_t adc1_handle;
-static adc_cali_handle_t adc1_cali_handle = NULL;
-    
-static bool calib1 = false;
+/*************************************************************************
+ * Print info about characterisation
+ *************************************************************************/
+
+void adc_print_char()
+{
+    if (char_type == ESP_ADC_CAL_VAL_EFUSE_TP_FIT)
+        printf("Characterized using Two point value FIT\n");
+    else if (char_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
+        printf("Characterized using Two Point Value\n");
+    } else if (char_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
+        printf("Characterized using eFuse Vref\n");
+    } else {
+        printf("Characterized using configured Vref\n");
+    }
+}
 
 
 /*************************************************************************
  * Set up ADC
  *************************************************************************/
 
+
 void adc_init()
 {
-    /* ADC1 Init*/
-    adc_oneshot_unit_init_cfg_t init_config1 = {
-        .unit_id = ADC_UNIT_1,
-        .ulp_mode = ADC_ULP_MODE_DISABLE,
-    };
-    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
-
-    /* Channels Config */
-    adc_oneshot_chan_cfg_t config = {
-        .bitwidth = WIDTH,
-        .atten = ATTEN,
-    };
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, X1_ADC_INPUT, &config));
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, BATT_ADC_INPUT, &config));
+    //Characterize ADC
+    uint16_t ref = get_u16_param("ADC.REF", DEFAULT_VREF);
+    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    char_type = esp_adc_cal_characterize(UNIT, ATTEN, WIDTH, ref, adc_chars);
+    adc_print_char();
     
-    /* RADIO INPUT is on ADC2 */    
-}
+    /* Configure ADC input channels */
+    adc1_config_width(WIDTH);
 
+#if !defined(RADIO_DISABLE)
+    adc2_config_channel_atten(RADIO_INPUT, ATTEN);
+#endif
+    adc1_config_channel_atten(X1_ADC_INPUT, ATTEN);
+    adc1_config_channel_atten(BATT_ADC_INPUT, ATTEN);
+    adc_calibrate(); 
+}
 
 
 /*************************************************************************
@@ -168,32 +119,34 @@ void adc_init()
  * to voltage
  *************************************************************************/
 
+
 uint16_t adc1_read(uint8_t chan) 
 { 
-    int raw=0;
-    ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, chan, &raw));
-    ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, chan, raw);
-    return raw;
+    uint32_t val = 0;
+    for (int i=0; i<64; i++)
+        val+= adc1_get_raw((adc1_channel_t) chan);
+    return (uint16_t) (val/64); 
 }
 
+uint16_t adc2_read(uint8_t chan) 
+{ 
+    return 0;
+    uint32_t val = 0;
+    int out, n = 0; 
+    for (int i=0; i<64; i++)
+        if (adc2_get_raw(WIDTH, (adc2_channel_t) chan, &out) == ESP_OK)
+            {n++; val += out;};
+    printf("adc2 n=%d\n", n);
+    return (uint16_t) (val/n); 
+}
 
 
 /*************************************************************************
  * Convert ADC reading to voltage
  *************************************************************************/
 
-uint16_t adc_toVoltage(uint16_t val) 
-{ 
-    int voltage=0; 
-    if (!calib1) 
-        calib1 = adc_calibration_init(ADC_UNIT_1, ATTEN, &adc1_cali_handle);
-    if (calib1) {
-        ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_handle, val, &voltage));
-        ESP_LOGI(TAG, "ADC Cali Voltage: %d mV", voltage);
-    }
-    return voltage;
-}
- 
+uint16_t adc_toVoltage(uint16_t val)
+    { return 0;  return (uint16_t) esp_adc_cal_raw_to_voltage((uint32_t) val, adc_chars); }
 
     
 /*************************************************************************
@@ -209,17 +162,14 @@ uint16_t adc_batt()
 }
 
 
-
-
 /*************************************************************************
  * Sampling of radio channel - to be called from timer ISR 
  *************************************************************************/
 
-
-void radio_adc_init()
-{
+void adc_calibrate() {
     /* Calibrate radio channel input */
-    dcoffset = adc2_read(RADIO_INPUT);
+    dcoffset = adc1_read(RADIO_INPUT);
+    printf("DCOFFSET=%d\n", dcoffset);
 }
 
 
@@ -230,7 +180,7 @@ int16_t radio_adc_sample()
      * spinlock and interrupts may happen there interfering with the read. 
      */
     taskDISABLE_INTERRUPTS();
-    uint16_t sample = (uint16_t) adc2_get_rawISR((adc2_channel_t) RADIO_INPUT);
+    uint16_t sample = sample = (uint16_t) adc1_get_raw((adc1_channel_t) RADIO_INPUT);
   
     taskENABLE_INTERRUPTS();    
     sample &= 0x0FFF;
