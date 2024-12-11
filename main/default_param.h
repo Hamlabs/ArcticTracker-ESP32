46888e605a32330d37ef00a638451dc3a80c7462
LA7ECA, Øyvind <oh@hans.priv.no>
2024-12-11 21:57:36 +0100

    Fix duplicate define

--- main/default_param.h
+++ main/default_param.h
@@ -16,7 +16,7 @@
 #define DFL_SOFTAP_PASSWD  "123456789"
 #define DFL_SOFTAP_IP      "192.168.0.1"
 
-
+ 
 #define DFL_LORA_SF          9
 #define DFL_LORA_CR          7
 #define DFL_REPEAT           0
@@ -41,7 +41,6 @@
 
 #define DFL_TRX_SQUELCH      1
 #define DFL_TRX_MICLEVEL     6
-#define DFL_TRX_VOLUME       6
 #define DFL_TURNLIMIT       35
 #define DFL_IGATE_PORT   14580 
 #define DFL_TXFREQ     1448000
