13e759824bb4a7bc1203f7695d6bece6a4dfcc82
LA7ECA, Øyvind <oh@hans.priv.no>
2026-09-07 21:34:08 +0200

    Fix DST problem when getting time from GNSS

--- main/gps.c
+++ main/gps.c
@@ -360,7 +360,7 @@ static void nmea2time( time_t* t, const char* timestr, const char* datestr)
     sscanf(timestr, "%2u%2u%2u", &hour, &min, &sec);
     sscanf(datestr, "%2u%2u%2u", &day, &month, &year);
     
-    struct tm tm; 
+    struct tm tm = {0}; 
     tm.tm_year = year + 2000 - 1900;
     tm.tm_mon = month - 1; 
     tm.tm_mday = day;
